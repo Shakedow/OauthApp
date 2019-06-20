8abe8794a23897f332d27039b0917bb8ac8699cf
Shakedow <49735667+Shakedow@users.noreply.github.com>
2019-06-19 19:20:20 -0600

    First

---
+++ config/passport-setup.js
@@ -0,0 +1,47 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./keys');
+const User=require('../models/user-model');
+
+passport.serializeUser((user,done)=>{
+  done(null, user.id);
+});
+passport.deserializeUser((id,done)=>{
+  User.findById(id).then((user)=>{
+    done(null, user);
+  });
+
+});
+
+
+passport.use(
+  new GoogleStrategy ({
+  //options for google strategy
+
+  clientID:keys.google.clientID,
+  clientSecret:keys.google.clientSecret,
+  callbackURL:'auth/google/redirect'
+}, (accessToken, refreshToken, profile, done) => {
+//check if user already exists in our db
+User.findOne({googleId:profile.id}).then((currentUser)=>{
+if(currentUser){
+  //already havea a user
+console.log('user is:', currentUser)
+done(null, currentUser);
+
+} else {
+  //if not create user in db
+
+  new User({
+    username:profile.displayName,
+    googleId:profile.id
+  }).save().then((newUser)=>{
+    console.log('new user created:' + newUser);
+    done(null, newUser);
+
+  });
+}
+});
+
+}
+));
