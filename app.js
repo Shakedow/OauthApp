0dfd2ee67972f6c2687591da78cda48fb6d88cb8
Shakedow <49735667+Shakedow@users.noreply.github.com>
2019-06-21 11:21:05 -0600

    Final canged and config

--- app.js
+++ app.js
@@ -1,35 +1,42 @@
 const express = require('express');
-const authRoutes = require('./routes/auth-routes');
-const passportSetup = require('./config/passport-setup');
-const profileRoutes = require('./routes/profile-routes');
-const app = express();
-const mongoose=require('mongoose');
-const keys=require('./config/keys');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const authRoutes = require('./routes/auth-routes');
+const profileRoutes = require('./routes/profile-routes');
+const passportSetup = require('./config/passport-setup');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
-//set up view engine
+const app = express();
+
+// set view engine
 app.set('view engine', 'ejs');
+
+// set up session cookies
 app.use(cookieSession({
-	maxAge: 24*60*60*1000,
-	keys:[keys.session.cookieKey]
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [keys.session.cookieKey]
 }));
 
-//initialize passport
+// initialize passport
 app.use(passport.initialize());
 app.use(passport.session());
 
-//connect to mongoose
-mongoose.connect(keys.mongodb.dbURI,() =>{console.log('connected to mongodb');});
-// set up routes
-app.use('/auth', authRoutes);
-app.use('/profile', profileRoutes);
-// create home route
-app.get('/', (req,res)=>{
-	res.render('home');
+
+// connect to mongodb
+mongoose.connect(keys.mongodb.dbURI, () => {
+    console.log('connected to mongodb');
 });
 
+// set up routes
+app.use('http://localhost:3000/auth', authRoutes);
+app.use('http://localhost:3000/profile', profileRoutes);
 
-app.listen(3000,()=> {
-	console.log('app now listening for requests on port 3000');
+// create home route
+app.get('/', (req, res) => {
+    res.render('home', { user: req.user });
+});
+
+app.listen(3000, () => {
+    console.log('app now listening for requests on port 3000');
 });
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,47 +1,43 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./keys');
-const User=require('../models/user-model');
+const User = require('../models/user-model');
 
-passport.serializeUser((user,done)=>{
-  done(null, user.id);
-});
-passport.deserializeUser((id,done)=>{
-  User.findById(id).then((user)=>{
-    done(null, user);
-  });
-
+passport.serializeUser((user, done) => {
+    done(null, user.id);
 });
 
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user);
+    });
+});
 
 passport.use(
-  new GoogleStrategy ({
-  //options for google strategy
+    new GoogleStrategy({
+        // options for google strategy
+        callbackURL:'http://localhost:3000/auth/google/redirect',
+        clientID: keys.google.clientID,
+        clientSecret: keys.google.clientSecret
 
-  clientID:keys.google.clientID,
-  clientSecret:keys.google.clientSecret,
-  callbackURL:'auth/google/redirect'
-}, (accessToken, refreshToken, profile, done) => {
-//check if user already exists in our db
-User.findOne({googleId:profile.id}).then((currentUser)=>{
-if(currentUser){
-  //already havea a user
-console.log('user is:', currentUser)
-done(null, currentUser);
-
-} else {
-  //if not create user in db
-
-  new User({
-    username:profile.displayName,
-    googleId:profile.id
-  }).save().then((newUser)=>{
-    console.log('new user created:' + newUser);
-    done(null, newUser);
-
-  });
-}
-});
-
-}
-));
+    }, (accessToken, refreshToken, profile, done) => {
+        // check if user already exists in our own db
+        User.findOne({googleId: profile.id}).then((currentUser) => {
+            if(currentUser){
+                // already have this user
+                console.log('user is: ', currentUser);
+                done(null, currentUser);
+            } else {
+                // if not, create user in our db
+                new User({
+                    googleId: profile.id,
+                    username: profile.displayName,
+                    thumbnail: profile._json.image.url
+                }).save().then((newUser) => {
+                    console.log('created new user: ', newUser);
+                    done(null, newUser);
+                });
+            }
+        });
+    })
+);
